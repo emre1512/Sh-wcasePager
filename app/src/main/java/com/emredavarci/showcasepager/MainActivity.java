2d65bd80f43cf2ac3e740d86dda5f82a632e959a
M. Emre Davarcı <emre151291@gmail.com>
2017-06-20 22:27:35 +0300

    Modified readme

--- app/src/main/java/com/emredavarci/showcasepager/MainActivity.java
+++ app/src/main/java/com/emredavarci/showcasepager/MainActivity.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.view.DragEvent;
 import android.view.View;
 import android.widget.AbsListView;
+import android.widget.AdapterView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -89,4 +90,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    
+
 }
